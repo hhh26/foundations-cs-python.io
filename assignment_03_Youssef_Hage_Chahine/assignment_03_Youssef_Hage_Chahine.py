a527d826ed87ed6675de0ee49cb5abeb39241e2b
hhh26 <y.h.ch26@gmail.com>
2023-07-20 11:20:13 +0300

    menu

--- assignment_03_Youssef_Hage_Chahine/assignment_03_Youssef_Hage_Chahine.py
+++ assignment_03_Youssef_Hage_Chahine/assignment_03_Youssef_Hage_Chahine.py
@@ -13,25 +13,24 @@ def sumTuples(tup1, tup2):
     sum_tup = tuple(list_sum)
     return sum_tup
 
-tup1 = ()
-tup2 = ()
-
-ln = int(input('enter the desire length: '))
-
-for i in range(ln):
-  lst1 = list(tup1)
-  i = int(input('enter number for tuple 1: '))
-  lst1.append(i)
-  tup1 = tuple(lst1)
-
-for j in range(ln):
-  lst2 = list(tup2)
-  j = int(input('enter a number for tuple 2: '))
-  lst2.append(j)
-  tup2 = tuple(lst2)
-
-print(tup1, tup2)
-print(sumTuples(tup1, tup2))
+#tup1 = ()
+#tup2 = ()
+#ln = int(input('enter the desire length: '))
+#
+#for i in range(ln):
+#  lst1 = list(tup1)
+#  i = int(input('enter number for tuple 1: '))
+#  lst1.append(i)
+#  tup1 = tuple(lst1)
+#
+#for j in range(ln):
+#  lst2 = list(tup2)
+#  j = int(input('enter a number for tuple 2: '))
+#  lst2.append(j)
+#  tup2 = tuple(lst2)
+#
+#print(tup1, tup2)
+#print(sumTuples(tup1, tup2))
 
 
 # ex 2:
@@ -54,11 +53,11 @@ def convert_dic_to_json(dictionary):
   json_data = json_data.rstrip(",") + '}'
   return json_data
 
-data = {
-  "name": "Youssef", 
-  "age": 31
-  }
-write_dic_to_json(data, 'assignment_03_Youssef_Hage_Chahine\ex2.json')
+#data = {
+#  "name": "Youssef", 
+#  "age": 31
+#  }
+#write_dic_to_json(data, 'assignment_03_Youssef_Hage_Chahine\ex2.json')
 
 
 # ex 3:
@@ -83,6 +82,78 @@ def convert_json_to_dict(filename):
   
   return lst2
 
-filename = 'assignment_03_Youssef_Hage_Chahine\ex3.json'
-json_to_dict = convert_json_to_dict(filename)
-print(json_to_dict)
+#filename = 'assignment_03_Youssef_Hage_Chahine\ex3.json'
+#json_to_dict = convert_json_to_dict(filename)
+#print(json_to_dict)
+
+
+
+############
+####Menu####
+############
+
+def sumTuples_menu():
+    tup1 = ()
+    tup2 = ()
+    ln = int(input('enter the desire length: '))
+
+    for i in range(ln):
+      lst1 = list(tup1)
+      i = int(input('enter number for tuple 1: '))
+      lst1.append(i)
+      tup1 = tuple(lst1)
+
+    for j in range(ln):
+      lst2 = list(tup2)
+      j = int(input('enter a number for tuple 2: '))
+      lst2.append(j)
+      tup2 = tuple(lst2)
+
+    print(tup1, tup2)
+    print(sumTuples(tup1, tup2))
+
+def convert_dic_to_json_menu():
+    data = {
+    "name": "Youssef", 
+    "age": 31
+    }
+    write_dic_to_json(data, 'assignment_03_Youssef_Hage_Chahine\ex2.json')
+
+def convert_json_to_dic_menu():
+    filename = 'assignment_03_Youssef_Hage_Chahine\ex3.json'
+    json_to_dict = convert_json_to_dict(filename)
+    print(json_to_dict)
+
+
+def displayMenu():
+    print("Menu:")
+    print("1. Sum Tuples")
+    print("2. Export JSON")
+    print("3. Import JSON")
+    print("4. Exit")
+
+def main():
+    displayMenu()
+    choice = int(input("please enter your choice here: "))
+
+    while choice != 4:
+        if choice == 1:
+            sumTuples_menu()
+        elif choice == 2:
+            convert_dic_to_json_menu()
+        elif choice == 3:
+            convert_json_to_dic_menu()
+        else:
+            print("this is an invalid choice ")
+        
+        
+        print(' - - - - - - - - ')
+
+        
+        displayMenu()
+        choice = int(input("please enter your choice here: "))
+    
+    print("Thank you for using my program!, you exited")
+    
+    
+main()
