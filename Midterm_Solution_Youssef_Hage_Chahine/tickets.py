8b1d408635b141acd4ae13fa71bea1a6694e227c
hhh26 <y.h.ch26@gmail.com>
2023-07-28 16:14:58 +0300

    book ticket + updated ticket list

--- Midterm_Solution_Youssef_Hage_Chahine/tickets.py
+++ Midterm_Solution_Youssef_Hage_Chahine/tickets.py
@@ -1,3 +1,7 @@
+import datetime
+
+
+
 # import the txt file :
 tickets_list = []
 with open('ticket_list.txt', 'r') as file:
@@ -13,7 +17,8 @@ for tik_data in ticket_data:
 #print(tickets_list)
 
 # Admin users
-# display staistics
+
+## display staistics
 
 
 def displayStatistics(tickets_list):
@@ -33,3 +38,34 @@ def displayStatistics(tickets_list):
 max_event_id, max_event_count = displayStatistics(tickets_list)
 
 print(f'Max event id: {max_event_id}, {max_event_count}')
+
+
+## Book a ticket:
+## 1st step admin input:
+username = input('enter a name: ')
+event_id = input('enetr the event id: ')
+priority = input('enter priority lvl from 0 to 3: ')
+
+
+### 2nd step get the curret date  
+def get_date():
+    return datetime.datetime.now().strftime('%Y%m%d') #https://www.programiz.com/python-programming/datetime/strftime# 
+# return the current time in this format yyyymmdd
+
+### 3rd step create next ticket id:
+def new_ticket():
+    with open('ticket_list.txt', 'r') as file:
+        last_ticket = file.readlines()[-1] # the last tikcket in the list 
+        last_ticket_id = int(last_ticket.split(', ')[0].lstrip('tick'))
+        return f"tick{last_ticket_id + 1:02}"
+
+current_date = get_date()
+new_ticket_id = new_ticket()
+
+new_ticket_list = f"{new_ticket_id}, {event_id}, {username}, {current_date}, {priority}\n"
+
+with open('ticket_list.txt', 'a') as file:
+    file.write(new_ticket_list)
+print('ticket list updated')
+
+
